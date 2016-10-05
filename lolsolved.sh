3ced95598c6828f119f42f3e8f9766aea40acfe0
David Barksdale <david.barksdale@exodusintel.com>
2016-10-05 15:30:38 -0500

    Harden more against whitespace and -

--- lolsolved.sh
+++ lolsolved.sh
@@ -9,34 +9,37 @@ fi
 
 featherduster='~/tools/featherduster/featherduster.py'
 
+files="$1"
+# resolve files so we don't have to worry about it beginning with '-'
+[ -z "${files%%/*}" ] || files="$PWD/$files"
 FLAG="$2"
-FLAG_REVERSE=`echo $FLAG | rev`
+FLAG_REVERSE="$(printf "%s" "$FLAG" | rev)"
 
 echo 'Magic CTF challenge solver GO!'
 echo ''
 
 # Do general solves
 # strings | grep
-find "$1" | while read file; do
-   strings "$file" | grep "$FLAG" && echo 'GETREKT: strings | grep flag' && exit 0
-   strings "$file" | grep "$FLAG_REVERSE" && echo 'GETREKT: strings | grep flag_in_reverse' && exit 0
+find "$files" | while read file; do
+   strings "$file" | grep -e "$FLAG" && echo 'GETREKT: strings | grep flag' && exit 0
+   strings "$file" | grep -e "$FLAG_REVERSE" && echo 'GETREKT: strings | grep flag_in_reverse' && exit 0
    binwalk -e "$file" >/dev/null;
-   grep -r -i "$FLAG" *.extracted/ && echo 'GETREKT: binwalk -e -M ; grep -r flag' && exit 0
-   grep -r -i "$FLAG_REVERSE" *.extracted/ && echo 'GETREKT: binwalk -e -M ; grep -r flag_in_reverse' && exit 0
+   grep -r -i -e "$FLAG" *.extracted/ && echo 'GETREKT: binwalk -e -M ; grep -r flag' && exit 0
+   grep -r -i -e "$FLAG_REVERSE" *.extracted/ && echo 'GETREKT: binwalk -e -M ; grep -r flag_in_reverse' && exit 0
 
    # Do crypto solves
    # FeatherDuster autopwn
-   python $featherduster "$file" <<EOF | grep "$FLAG" && echo 'GETREKT: featherduster autodecode' && exit 0
+   python $featherduster "$file" <<EOF | grep -e "$FLAG" && echo 'GETREKT: featherduster autodecode' && exit 0
 analyze
 samples
 EOF
 
    # FeatherDuster autodecode
-   echo 'autopwn' | python "$featherduster" "$file" | grep "$FLAG" && echo 'GETREKT: featherduster autopwn' && exit 0
+   echo 'autopwn' | python "$featherduster" "$file" | grep -e "$FLAG" && echo 'GETREKT: featherduster autopwn' && exit 0
    
 
    # Do reversing solves
-   rabin2 -zz "$file" | grep -i "$FLAG" && exit 0
+   rabin2 -zz "$file" | grep -i -e "$FLAG" && exit 0
 
    # Do stego solves
    #TODO Do stego solves
